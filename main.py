6022ededdaf4f69f2de2fd88dc0c50768edf02cb
jhoanmartinez <jhoao1320@gmail.com>
2023-08-01 12:43:42 -0500

    partil derivates

--- main.py
+++ main.py
@@ -28,10 +28,23 @@ dsum_dxw0 = 1
 dsum_dxw1 = 1
 dsum_dxw2 = 1
 dsum_db = 1
-
 drelu_dxw0 = drelu_dz * dsum_dxw0
-drelu_dxw1 = drelu_dz * dsum_dxw1                                                                                                                                                                                                                                                                                                                                                                                                                        
-drelu_dxw2 = drelu_dz * dsum_dxw2                                                                                                                                                                                                                                                                                                                                                                                                                        
+drelu_dxw1 = drelu_dz * dsum_dxw1
+drelu_dxw2 = drelu_dz * dsum_dxw2
 drelu_db = drelu_dz * dsum_db
-
 print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
+
+# Partial derivatives of the multiplication, the chain rule
+dmul_dx0 = w[0]
+dmul_dx1 = w[1]
+dmul_dx2 = w[2]
+dmul_dw0 = x[0]
+dmul_dw1 = x[1]
+dmul_dw2 = x[2]
+drelu_dx0 = drelu_dxw0 * dmul_dx0
+drelu_dw0 = drelu_dxw0 * dmul_dw0
+drelu_dx1 = drelu_dxw1 * dmul_dx1
+drelu_dw1 = drelu_dxw1 * dmul_dw1
+drelu_dx2 = drelu_dxw2 * dmul_dx2
+drelu_dw2 = drelu_dxw2 * dmul_dw2
+print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
